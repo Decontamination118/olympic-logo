d43c93f2cddd5b9c55f3886977c4bbfa4a6562f0
Decontamination118 <72770175+Decontamination118@users.noreply.github.com>
2020-12-02 14:37:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+
+ctx.beginPath();
+ctx.strokeStyle="grey";
+ctx.lineWidth=4;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth=5;
+ctx.arc(250, 210, 40, 0, 2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=5;
+ctx.arc(330, 210, 40, 0, 2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=5;
+ctx.arc(410, 210, 40, 0, 2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.lineWidth=5;
+ctx.arc(450, 230, 40, 0, 2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=5;
+ctx.arc(490, 230, 40, 0, 2);
+ctx.stroke();
